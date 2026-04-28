c50f7ad045134de16a7830546f7a8693e8d14db3
Hamit Mızrak <hamitmizrak@gmail.com>
2026-04-28 15:25:33 +0300

    Quiz

---
+++ python_intermedia/Quiz.py
@@ -0,0 +1,15 @@
+"""
+Bu dosya, terminal üzerinden çalışan gelişmiş bir Quiz / Test uygulamasıdır.
+
+Amaç:
+- Soruları CSV dosyasından okumak
+- Kullanıcıya soruları sırayla sormak
+- Cevapları kontrol etmek
+- Sonuçları TXT, CSV ve HTML formatında raporlamak
+- Soruları rastgele karıştırarak daha dinamik bir deneyim sunmak
+
+Not:
+Bu sürümde mevcut çalışan yapı korunmuştur.
+Sadece kodun anlaşılmasını kolaylaştırmak için komutların ve blokların üstüne
+çok detaylı açıklamalar eklenmiştir.
+"""
